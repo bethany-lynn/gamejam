b7e202a54793385cc38557fbab9585e3dae8bb71
bethany <bethanylynnpro@gmail.com>
2023-07-30 08:15:06 -0700

    added text to endscreen

--- src/ui_components/EndScreen.js
+++ src/ui_components/EndScreen.js
@@ -4,6 +4,11 @@ import './EndScreen.css';
 export default function EndScreen({ onRestartGame }) {
   return (
     <div className="EndScreen">
+      <div className="end-text">
+        Oh no, she's flown into a balloon and got stuck! 
+        <br/>
+        Lil Bird is okay, but needs to get home still!
+      </div>
       <button onClick={onRestartGame} id="button">Play Again</button>
     </div>
   );
